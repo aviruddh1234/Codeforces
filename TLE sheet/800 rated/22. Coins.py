fd9ec11ed714ccb458bf8994d241dca1d2fe0cb3
Aviruddh Singhal <aviruddhsinghal@gmail.com>
2025-08-27 12:06:56 +0530

    new

---
+++ TLE sheet/800 rated/22. Coins.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for _ in range(t):
+  n, k = map(int,input().split())
+  
+  if k == 1 or n % 2 == 0:
+    print("YES")
+  
+  elif (n-k)%2 == 0:
+    print("YES")
+  
+  else:
+    print("NO")
