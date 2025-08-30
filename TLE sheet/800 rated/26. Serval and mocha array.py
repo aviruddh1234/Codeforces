60f91fc99b5162bd6f4cdc687bde93ecbe50b5a2
Aviruddh Singhal <aviruddhsinghal@gmail.com>
2025-08-30 14:07:50 +0530

    new

---
+++ TLE sheet/800 rated/26. Serval and mocha array.py
@@ -0,0 +1,28 @@
+def gcd(a,b):
+  if a == 0:
+    return b 
+  else:
+    return gcd(b%a,a)
+    
+t = int(input())
+for _ in range(t):
+  n = int(input())
+  arr = list(map(int,input().split()))
+  
+  flag = True 
+  
+  for i in range(n):
+    for j in range(i+1,n):
+      a = max(arr[i],arr[j])
+      b = min(arr[i],arr[j])
+      
+      if gcd(a,b) <= 2:
+        flag = False 
+        break
+    
+  
+  if flag:
+    print("No")
+  else:
+    print("Yes")
+      
